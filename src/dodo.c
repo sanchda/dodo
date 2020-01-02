0383e483c04f52eae1cfaee4cbf39392f26beda7
David Sanchez <sanchda@gmail.com>
2020-01-02 00:02:47 -0500

    light refactor
    Standardize function names to include full struct name of target arg

--- src/dodo.c
+++ src/dodo.c
@@ -15,7 +15,74 @@
 #include <sys/time.h>
 #include <pwd.h>
 
+/******************************************************************************\
+|                                   Helpers                                    |
+\******************************************************************************/
+struct timeval tv_now     = {0};  // hold timevalified time
+char           ts_now[21] = {0};  // hold stringified unix time
+time_t         tt_now     = {0};  // hold regular unix time
+struct tm      tm_now     = {0};  // hold broken-down time of now
 
+void time_init() {
+  // Slightly inaccurate, since the times won't be identical between the first two calls
+  tt_now = time(NULL);
+  gettimeofday(&tv_now, NULL);
+  sprintf(ts_now, "%lu", tt_now);
+  strptime(ts_now, "%s", &tm_now);
+}
+
+static inline int days_diff(time_t a, time_t b) {
+  return (a>b ? a-b : b-a)/86400;
+}
+
+static inline char hasterm(char* path) {return '/' == path[strlen(path)-1];}
+
+
+/******************************************************************************\
+|                              VT-100 Formatters                               |
+\******************************************************************************/
+#define FG(x) "\33[3"#x"m"
+#define BG(x) "\33[4"#x"m"
+#define AT(x) "\33[" #x"m"
+#define CLS   "\33[0m\33[39m\33[49m"
+char*F[]={FG(0),FG(1),FG(2),FG(3),FG(4),FG(5),FG(6),FG(7),FG(8),FG(9)};
+char*B[]={BG(0),BG(1),BG(2),BG(3),BG(4),BG(5),BG(6),BG(7),BG(8),BG(9)};
+char*A[]={AT(0),
+  AT(1),
+  AT(2),
+  AT(1) AT(2),
+  AT(4),
+  AT(4) AT(1),
+  AT(4) AT(2),
+  AT(4) AT(1) AT(2),
+  AT(5),
+  AT(5) AT(1),
+  AT(5) AT(2),
+  AT(5) AT(1) AT(2),
+  AT(5) AT(4),
+  AT(5) AT(4) AT(1),
+  AT(5) AT(4) AT(2),
+  AT(5) AT(4) AT(1) AT(2)};
+
+typedef enum sp_type {
+  SP_DONE = 1<<0,
+  SP_HIPR = 1<<1,
+  SP_STAR = 1<<2,
+  SP_LOVE = 1<<3,
+  SP_NOTE = 1<<4
+} sp_type;
+
+static char sp_todo[] = FG(5)"□"FG(9);
+static char sp_done[] = FG(2)"✓"FG(9);
+static char sp_hipr[] = FG(6)"!"FG(9);
+static char sp_star[] = FG(3)"★"FG(9);
+static char sp_love[] = FG(1)"♡"FG(9);
+static char sp_note[] = FG(4)"•"FG(9);
+
+
+/******************************************************************************\
+|                                DDNode, DDList                                |
+\******************************************************************************/
 #define MAX_INDENT 16
 #define MAX_MODS   5
 
@@ -43,28 +110,7 @@ size_t DDListLen(DDList* dl) {
   return n;
 }
 
-// Default time globals
-struct timeval tv_now     = {0};  // hold timevalified time
-char           ts_now[21] = {0};  // hold stringified unix time
-time_t         tt_now     = {0};  // hold regular unix time
-struct tm      tm_now     = {0};  // hold broken-down time of now
-
-void time_init() {
-  // Slightly inaccurate, since the times won't be identical between the first two calls
-  tt_now = time(NULL);
-  gettimeofday(&tv_now, NULL);
-  sprintf(ts_now, "%lu", tt_now);
-  strptime(ts_now, "%s", &tm_now);
-}
-
-int days_diff(time_t a, time_t b) {
-  return (a>b ? a-b : b-a)/86400;
-}
-
-
-static inline char hasterm(char* path) {return '/' == path[strlen(path)-1];}
-
-char* DDLGetMakeRoot() {
+char* DDListGetMakeRoot() {
   static char dodo_subdir[] = ".dodo/";
   char *rootd = NULL, *buf = NULL;
   DIR* dir;
@@ -111,12 +157,12 @@ char* DDLGetMakeRoot() {
   return rootd;
 }
 
-char* DDLGetMakeStash() {
+char* DDListGetMakeStash() {
   static char dodo_stash[] = "stash/";
   char *stashd;
   DIR* dir;
   if(!DODO_ROOT) {
-    if(!DDLGetMakeRoot()) return NULL;
+    if(!DDListGetMakeRoot()) return NULL;
   }
   stashd = calloc(1,strlen(DODO_ROOT) + 1 + strlen(dodo_stash) + 1);
   strcpy(stashd, DODO_ROOT);
@@ -146,13 +192,13 @@ void DDListToFD(DDList* ddl, int fd) {
   }
 }
 
-size_t DDLToFile(DDList* ddl, char* name) {
+size_t DDListToFile(DDList* ddl, char* name) {
   static char dodo_temp[] = "DODO_TEMP_FILE_PLEASE_IGNORE";
   int fd = -1, l=0;
   size_t sz_pl = 0;
   char *fpath = NULL, *ftemp = NULL;
   unsigned char *payload = NULL, *p = NULL;
-  if(!DDLGetMakeStash()) return 0;
+  if(!DDListGetMakeStash()) return 0;
   fpath = calloc(1, strlen(DODO_STASH) + 1 + strlen(name) + 1);
   strcpy(fpath, DODO_STASH);
   if(!hasterm(DODO_STASH)) strcat(fpath, "/");
@@ -187,7 +233,7 @@ size_t DDLToFile(DDList* ddl, char* name) {
   return sz_pl;
 }
 
-DDList* FileToDDL(char* name) {
+DDList* FileToDDList(char* name) {
   static char T[256] = {0};T['*']=T['-']=T['+']=T['.']=T['>']=T['<']=T['#']=T[' ']=T['\t']=1;
   DDList* ddl;
   char* fpath = NULL;
@@ -195,7 +241,7 @@ DDList* FileToDDL(char* name) {
   int fd = -1;
   struct stat sa = {0};
   time_init();
-  if(!DDLGetMakeStash()) return NULL;
+  if(!DDListGetMakeStash()) return NULL;
 
   fpath = calloc(1, strlen(DODO_STASH) + 1 + strlen(name) + 1);
   strcpy(fpath, DODO_STASH);
@@ -288,78 +334,39 @@ DDList* FileToDDL(char* name) {
 }
 
 
-#define FG(x) "\33[3"#x"m"
-#define BG(x) "\33[4"#x"m"
-#define AT(x) "\33[" #x"m"
-#define CLS   "\33[0m\33[39m\33[49m"
-char*F[]={FG(0),FG(1),FG(2),FG(3),FG(4),FG(5),FG(6),FG(7),FG(8),FG(9)};
-char*B[]={BG(0),BG(1),BG(2),BG(3),BG(4),BG(5),BG(6),BG(7),BG(8),BG(9)};
-char*A[]={AT(0),
-  AT(1),
-  AT(2),
-  AT(1) AT(2),
-  AT(4),
-  AT(4) AT(1),
-  AT(4) AT(2),
-  AT(4) AT(1) AT(2),
-  AT(5),
-  AT(5) AT(1),
-  AT(5) AT(2),
-  AT(5) AT(1) AT(2),
-  AT(5) AT(4),
-  AT(5) AT(4) AT(1),
-  AT(5) AT(4) AT(2),
-  AT(5) AT(4) AT(1) AT(2)};
-
-typedef enum sp_type {
-  SP_DONE = 1<<0,
-  SP_HIPR = 1<<1,
-  SP_STAR = 1<<2,
-  SP_LOVE = 1<<3,
-  SP_NOTE = 1<<4
-} sp_type;
-
+/******************************************************************************\
+|                        DDNode, DDList Print Functions                        |
+\******************************************************************************/
 void printn(DDNode* dn, int depth) {
-  static char box[]     = "□";
-  static char check[]   = "✓";
-  static char bullet[]  = "•";
-  static char heart[]   = "♡";
-  static char star[]    = "★";
-  static char sp_todo[] = "\33[35m□\33[39m";
-  static char sp_done[] = "\33[32m✓\33[39m";
-  static char sp_hipr[] = "\33[36m!\33[39m";
-  static char sp_star[] = "\33[33m★\33[39m";
-  static char sp_love[] = "\33[31m♡\33[39m";
-  static char sp_note[] = "\33[34m•\33[39m";
   char bg=9, fg=9, sp=0; int  at=0;
   for(int i=0; i<MAX_MODS; i++) {
     switch(dn->mods[i]) {
       // TODO this is dumb
       // lowercase => fg
-      case 'k': fg = 0;     break;
-      case 'r': fg = 1;     break;
-      case 'g': fg = 2;     break;
-      case 'y': fg = 3;     break;
-      case 'b': fg = 4;     break;
-      case 'm': fg = 5;     break;
-      case 'c': fg = 6;     break;
-      case 'w': fg = 7;     break;
+      case 'k': fg = 0;        break;
+      case 'r': fg = 1;        break;
+      case 'g': fg = 2;        break;
+      case 'y': fg = 3;        break;
+      case 'b': fg = 4;        break;
+      case 'm': fg = 5;        break;
+      case 'c': fg = 6;        break;
+      case 'w': fg = 7;        break;
       // uppercase => bg
-      case 'K': bg = 0;     break;
-      case 'R': bg = 1;     break;
-      case 'G': bg = 2;     break;
-      case 'Y': bg = 3;     break;
-      case 'B': bg = 4;     break;
-      case 'M': bg = 5;     break;
-      case 'C': bg = 6;     break;
-      case 'W': bg = 7;     break;
+      case 'K': bg = 0;        break;
+      case 'R': bg = 1;        break;
+      case 'G': bg = 2;        break;
+      case 'Y': bg = 3;        break;
+      case 'B': bg = 4;        break;
+      case 'M': bg = 5;        break;
+      case 'C': bg = 6;        break;
+      case 'W': bg = 7;        break;
       // numerics => display attributes
-      case '0': at  = 0;    break;
-      case '1': at |= 1<<0; break;
-      case '2': at |= 1<<1; break;
-      case '4': at |= 1<<2; break;
-      case '5': at |= 1<<3; break;
-      case '7': at |= 1<<4; break;
+      case '0': at  = 0;       break;
+      case '1': at |= 1<<0;    break;
+      case '2': at |= 1<<1;    break;
+      case '4': at |= 1<<2;    break;
+      case '5': at |= 1<<3;    break;
+      case '7': at |= 1<<4;    break;
       // others => special decorators
       case '$': sp |= SP_DONE; break;  // done
       case '!': sp |= SP_HIPR; break;  // hipri
@@ -393,10 +400,14 @@ void printd(DDList* ddl) {
     printn(&ddl->nodes[i], 1+2*ddl->depth[i]);
 }
 
+
+/******************************************************************************\
+|                                  Entrypoint                                  |
+\******************************************************************************/
 int main(int argc, char *argv[]) {
   if(argc < 2) return -1;
-  DDList* ddl = FileToDDL(argv[1]);
+  DDList* ddl = FileToDDList(argv[1]);
   printd(ddl);
-  DDLToFile(ddl, argv[1]);
+  DDListToFile(ddl, argv[1]);
   return 0;
 }
